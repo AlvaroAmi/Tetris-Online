e84e391b706ddd63338e2c7dc588d3f22222afc1
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-04-08 17:37:34 +0200

    Fix database.h function declarations

--- include/database.h
+++ include/database.h
@@ -3,7 +3,8 @@
 
 #include "sqlite3.h"
 
-int insert_user(sqlite3 *db, const char *username, const char *email, const char *password, int highestScore);
+int insert_user(sqlite3 *db, const char *username, const char *email, const char *password, int highest_score);
 int delete_user(sqlite3 *db, const char *username);
+int show_user(sqlite3 *db, const char *username, const char *password);
 
 #endif // DATABASE_H
