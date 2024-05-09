75aab6f4090953aa00a1faae69f3daa60a5495a3
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-05-10 01:00:22 +0200

    Initialize correct display colors

--- src/display.cpp
+++ src/display.cpp
@@ -1,14 +1,14 @@
 #if defined(_WIN32) || defined(_WIN64)
-    #define NCURSES_STATIC
-    #include <ncurses/ncurses.h>
+#define NCURSES_STATIC
+#include <ncurses/ncurses.h>
 #elif defined(__APPLE__) || defined(__linux__)
-    #include <ncurses.h>
+#include <ncurses.h>
 #else
-    #error "Unsupported operating system"
+#error "Unsupported operating system"
 #endif
 
+#include "tetris/tetromino.hpp"
 #include <iostream>
-#include <sstream>
 using namespace std;
 
 #define WIN_HEIGHT 46
@@ -18,7 +18,8 @@ using namespace std;
 #define MATRIX_COLS 10
 #define MATRIX_ROWS 20
 
-int paint_matrix(WINDOW* win, int (*matrix)[10]);
+void init_colors(void);
+int paint_matrix(WINDOW *win, int (*matrix)[10]);
 
 int main() {
     // WINDOWS
@@ -31,7 +32,7 @@ int main() {
     WINDOW *next_win;
     WINDOW *next_display_win;
 
-    //Test Matrix (To be removed in a future)
+    // Test Matrix (To be removed in a future)
     int matrix[MATRIX_ROWS][MATRIX_COLS] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -54,7 +55,7 @@ int main() {
         {0, 7, 7, 6, 0, 2, 4, 4, 5, 0},
         {1, 1, 1, 1, 2, 2, 4, 4, 0, 0}};
 
-    //Screen initialization
+    // Screen initialization
     initscr();
     if (LINES < WIN_HEIGHT || COLS < WIN_WIDTH) {
         endwin();
@@ -67,6 +68,8 @@ int main() {
     keypad(stdscr, TRUE); // Allow for arrow key input
     curs_set(0);          // Hide cursor
 
+    init_colors();
+
     outer_win = newwin(WIN_HEIGHT, WIN_WIDTH, START_X, START_Y);
     display_win = newwin(42, 42, 2, 4);
     title_win = newwin(10, 50, 4, 50);
@@ -78,7 +81,7 @@ int main() {
 
     next_win = newwin(13, 46, 29, 51);
     next_display_win = newwin(8, 16, 32, 67);
-    
+
     refresh();
 
     mvwprintw(title_win, 0, 0, "  ////////////////////////////////////////////  \n"
@@ -91,7 +94,7 @@ int main() {
                                "                                                 \n"
                                "  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  \n"
                                "  ////////////////////////////////////////////  \n");
-    
+
     wattron(score_win, A_BOLD);
     wattron(level_win, A_BOLD);
     wattron(lines_win, A_BOLD);
@@ -104,7 +107,7 @@ int main() {
                               "    \t\tNEXT TETROMINO:                           \n");
 
     mvwprintw(next_win, 12, 0, " \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
-    
+
     box(outer_win, 0, 0);
     box(display_win, 0, 0);
     box(score_win, 0, 0);
@@ -123,60 +126,60 @@ int main() {
 
     paint_matrix(display_win, matrix);
 
-    getch(); 
+    getch();
 
     endwin();
     return 0;
-} 
+}
 
-int paint_matrix(WINDOW* win, int (*matrix)[10]) 
-{
+void init_colors(void) {
+    if (!has_colors()) {
+        endwin();
+        fprintf(stderr, "Error - no color support on this terminal\n");
+        exit(1);
+    }
+
     start_color();
-    assume_default_colors(COLOR_WHITE, COLOR_BLACK);
+    init_pair(Color::CYAN, COLOR_CYAN, COLOR_BLACK);
+    init_pair(Color::BLUE, COLOR_BLUE, COLOR_BLACK);
+    init_pair(Color::ORANGE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(Color::YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(Color::GREEN, COLOR_GREEN, COLOR_BLACK);
+    init_pair(Color::PURPLE, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(Color::RED, COLOR_RED, COLOR_BLACK);
 
-    // COLOR-PAIR INITIALIZATION
-    // I Tetrominoid: Cyan (1)
-    init_pair(1, COLOR_BLACK, COLOR_CYAN);
-    // J Tetrominoid: Blue (2)
-    init_pair(2, COLOR_BLACK, COLOR_BLUE);
-    // L Tetrominoid: Orange/White (3)
-    init_pair(3, COLOR_BLACK, COLOR_WHITE);
-    // O Tetrominoid: Yellow (4)
-    init_pair(4, COLOR_BLACK, COLOR_YELLOW);
-    // S Tetrominoid: Green (5)
-    init_pair(5, COLOR_BLACK, COLOR_GREEN);
-    // T Tetrominoid: Purple (6)
-    init_pair(6, COLOR_BLACK, COLOR_MAGENTA);
-    // Z Tetrominoid: Red (7)
-    init_pair(7, COLOR_BLACK, COLOR_RED);
-    // Default
-    init_pair(9, COLOR_BLACK, COLOR_BLACK);
-    
+    // In case the terminal supports custom colors
+    init_color(Color::ORANGE, 1000, 500, 0);
+    init_color(Color::YELLOW, 920, 800, 180);
+}
+
+int paint_matrix(WINDOW *win, int (*matrix)[10]) {
+    start_color();
+    wattron(win, A_REVERSE);
+
     int row;
     int column;
-    for(row = 0; row < MATRIX_ROWS; row++)
-    {
-        for(column = 0; column < MATRIX_COLS; column++)
-        {
+    for (row = 0; row < MATRIX_ROWS; row++) {
+        for (column = 0; column < MATRIX_COLS; column++) {
             int value = matrix[row][column];
-            int x_coordinate = 1 + column*4;
-            int y_coordinate = 1 + row*2;
+            int x_coordinate = 1 + column * 4;
+            int y_coordinate = 1 + row * 2;
 
-            if(value == 0) {
+            if (value == 0) {
                 wattron(win, COLOR_PAIR(9));
                 mvwaddstr(win, y_coordinate, x_coordinate, "    ");
                 mvwaddstr(win, y_coordinate + 1, x_coordinate, "    ");
-                wattroff(win, COLOR_PAIR(9)); 
+                wattroff(win, COLOR_PAIR(9));
             } else {
                 wattron(win, COLOR_PAIR(value));
                 mvwaddstr(win, y_coordinate, x_coordinate, "    ");
                 mvwaddstr(win, y_coordinate + 1, x_coordinate, "    ");
-                wattroff(win, COLOR_PAIR(value)); 
+                wattroff(win, COLOR_PAIR(value));
             }
 
             wrefresh(win);
         }
     }
-    
+    wattroff(win, A_REVERSE);
     return 0;
 }
