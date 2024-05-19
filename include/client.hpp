3649b5a8f3af6074ae0f145f2fd773c571693130
AlvaroAmi <alvaro.amilibia9@gmail.com>
2024-05-19 18:28:46 +0200

    Fix client.hpp

--- include/client.hpp
+++ include/client.hpp
@@ -7,14 +7,6 @@
 
 using std::string;
 
-void send_request(SOCKET sock, const char* command, const char* email, const char* password, const char* username);
-int receive_response(SOCKET sock);
-void clear_screen();
-int has_uppercase(const char *str);
-int has_special_char(const char *str);
-int contains_at_symbol(const char *email);
-void start_listener_thread(SOCKET sock);
-void stop_listener_thread();
 void send_game_start(SOCKET sock, int user_id);
 void send_game_finish(SOCKET sock);
 void send_game_update(SOCKET sock, const string& matrix);
