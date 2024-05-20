b154f9040c6d9da703cd740546aac2b56f28df01
irunefen <irunefen.fernandez@opendeusto.es>
2024-05-20 02:00:11 +0200

    Add socket to singleplayer

--- include/tetris/singleplayer_tetris_game.hpp
+++ include/tetris/singleplayer_tetris_game.hpp
@@ -7,19 +7,21 @@
 #include "tetris/srs_manager.hpp"
 #include "tetris/tetris_game.hpp"
 #include "tetris/tetromino.hpp"
+#include <winsock2.h>
 
 class SingleplayerTetrisGameRenderer;
 
 class SingleplayerTetrisGame : TetrisGame {
     friend class SingleplayerTetrisGameRenderer;
     static constexpr int GRAVITY[MAX_DEFINED_LEVEL + 1] = {48, 43, 38, 33, 28, 23, 18, 13, 8, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1};
+    SOCKET sock;
 
     const SingleplayerTetrisGameRenderer &renderer;
     unsigned int score = 0;
     int totalLinesCleared = 0;
 
 public:
-    SingleplayerTetrisGame();
+    SingleplayerTetrisGame(SOCKET sock);
 
 private:
     unsigned int getLevel() const;
--- src/tetris/singleplayer_tetris_game.cpp
+++ src/tetris/singleplayer_tetris_game.cpp
@@ -6,11 +6,12 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
-
+#include "client.hpp"
 #include "tetris/singleplayer_tetris_game.hpp"
 #include "tetris/singleplayer_tetris_game_renderer.hpp"
+#include "database.h"
 
-SingleplayerTetrisGame::SingleplayerTetrisGame() : renderer(*this) {
+SingleplayerTetrisGame::SingleplayerTetrisGame(SOCKET sock) : sock(sock), renderer(*this) {
     nextTetrominoType = bagRandomGenerator.getNextTetrominoType();
     spawnTetromino();
     resetTicksTillGravity();
@@ -32,6 +33,17 @@ void SingleplayerTetrisGame::gameOver() {
     gameIsOver = true;
     // TODO: Implement game over screen
     // TODO: Save data to server
+    endTime = std::chrono::system_clock::now();
+
+    std::time_t start_time_t = std::chrono::system_clock::to_time_t(startTime);
+    std::time_t end_time_t = std::chrono::system_clock::to_time_t(endTime);
+
+    char start_str[20];
+    char end_str[20];
+    std::strftime(start_str, sizeof(start_str), "%Y-%m-%d %H:%M:%S", std::localtime(&start_time_t));
+    std::strftime(end_str, sizeof(end_str), "%Y-%m-%d %H:%M:%S", std::localtime(&end_time_t));
+
+    send_singleplayer_game_finish(sock, start_str, end_str, score, totalLinesCleared, getLevel());
 }
 
 unsigned int SingleplayerTetrisGame::getLevel() const {
