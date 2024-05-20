d2ff49a72885bcb582b697d11df3dd5f12547ad2
jaimeetxebarria <jaimeetxebarria@gmail.com>
2024-05-20 22:50:23 +0200

    It wasnt the last commit

--- src/tetris/multiplayer_tetris_game.cpp
+++ src/tetris/multiplayer_tetris_game.cpp
@@ -24,7 +24,7 @@ void MultiplayerTetrisGame::startGame() {
         sendPlayfield();
         renderer.render();
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
 
     endwin();
